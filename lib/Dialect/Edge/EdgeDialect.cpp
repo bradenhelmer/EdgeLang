478c62f727374e28d47da28a859ccc611db184ee
Braden Helmer <bradenhelmeraus@gmail.com>
2024-01-08 15:45:18 -0500

    Added a verifier

--- lib/Dialect/Edge/EdgeDialect.cpp
+++ lib/Dialect/Edge/EdgeDialect.cpp
@@ -7,8 +7,24 @@
 
 #include <Edge/Dialect/Edge/EdgeDialect.cpp.inc>
 
+#define CONSTANT_OP_WIDTH 64
+
 using namespace mlir;
 using namespace edge;
+
+namespace {
+static constexpr llvm::StringRef getSignedSemanticsString(
+    IntegerType::SignednessSemantics status) {
+  switch (status) {
+    case IntegerType::Signed:
+      return "signed";
+    case IntegerType::Unsigned:
+      return "unsigned";
+    case IntegerType::Signless:
+      return "signless";
+  }
+}
+}  // namespace
 
 void EdgeDialect::initialize() {
   addOperations<
@@ -17,11 +33,30 @@ void EdgeDialect::initialize() {
       >();
 }
 
+// ConstantOp
+
 void ConstantOp::build(::mlir::OpBuilder &odsBuilder,
                        ::mlir::OperationState &odsState, int64_t value) {
-  auto dataType = IntegerType::get(odsBuilder.getContext(), 64);
+  auto dataType = IntegerType::get(odsBuilder.getContext(), CONSTANT_OP_WIDTH);
   auto attr = IntegerAttr::get(dataType, value);
   ConstantOp::build(odsBuilder, odsState, dataType, attr);
+}
+
+LogicalResult ConstantOp::verify() {
+  auto type = getResult().getType();
+  auto result = llvm::dyn_cast<IntegerType>(type);
+  auto width = type.getWidth() == CONSTANT_OP_WIDTH;
+  auto signedSemantics =
+      type.getSignedness() == IntegerType::SignednessSemantics::Signed;
+  if (!result && width && signedSemantics) {
+    return success();
+  } else {
+    return emitOpError(
+               "ConstantOp could not be verified as a 64 bit signed integer! "
+               "It was a ")
+           << type.getWidth() << " bit "
+           << getSignedSemanticsString(type.getSignedness()) << " integer!";
+  }
 }
 
 #define GET_OP_CLASSES
