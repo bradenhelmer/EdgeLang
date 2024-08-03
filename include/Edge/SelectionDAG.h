8d4cc66c0021772f363c87837a24285fc68eae4f
Braden Helmer <bradenhelmeraus@gmail.com>
2024-08-03 04:14:39 -0400

    All selection methods implemented

--- include/Edge/SelectionDAG.h
+++ include/Edge/SelectionDAG.h
@@ -9,7 +9,9 @@
 #include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/Instructions.h>
 
+#include <sstream>
 #include <unordered_map>
+#include <vector>
 
 namespace edge {
 
@@ -25,44 +27,42 @@ enum class X86SelectionDAGNodeType : uint32_t {
   DIV,
   // Value Types
   CONSTANT,
-  REGISTER
+  REGISTER,
+  // Unknown
+  UNKNOWN
 };
 
 class SelectionDAGNode;
+class SelectionDAGValue;
+class StackAllocSelectionDAGNode;
 
 // Represents an edge in the SelectionDAG between nodes.
 class SelectionDAGUse {
-  SelectionDAGUse *Next;
-  SelectionDAGUse *Prev;
+  SelectionDAGValue *Val;
   SelectionDAGNode *User;
 
  public:
-  SelectionDAGUse() = default;
-};
-
-// Wrapper for a node producing a runtime 'value', this could be:
-// 	- A pointer
-// 	- Result of an instruction
-class SelectionDAGValue {
-  SelectionDAGNode *Node;
-
- public:
-  SelectionDAGValue(SelectionDAGNode *Node) : Node(Node) {}
-  virtual ~SelectionDAGValue() = default;
-  SelectionDAGNode *getNode() const { return Node; }
+  SelectionDAGUse(SelectionDAGValue *Val, SelectionDAGNode *User)
+      : Val(Val), User(User) {}
+  ~SelectionDAGUse() = default;
 };
 
 // Base node class for nodes in SelectionDAG
 class SelectionDAGNode : public llvm::ilist_node<SelectionDAGNode> {
   X86SelectionDAGNodeType type;
 
-  SelectionDAGUse *Operands;
+  SelectionDAGUse *Operands[3];
+  uint8_t OpCount = 0;
 
  public:
   SelectionDAGNode(X86SelectionDAGNodeType type) : type(type) {}
-  ~SelectionDAGNode() = default;
+  ~SelectionDAGNode() {
+    for (int i = 0; i < OpCount; ++i) delete Operands[i];
+  }
 
   X86SelectionDAGNodeType getType() const { return type; }
+  void addOperand(SelectionDAGUse *Use) { Operands[OpCount++] = Use; }
+  SelectionDAGUse *getOperand(uint8_t index) { return Operands[index]; }
 };
 
 class ConstantSelectionDAGNode : public SelectionDAGNode {
@@ -89,9 +89,55 @@ class StackAllocSelectionDAGNode : public SelectionDAGNode {
         Name(Name),
         SizeInBytes(SizeInBytes),
         Offset(Offset) {}
-  const llvm::StringRef getName() const { return Name; }
+  const std::string &getName() const { return Name; }
   int32_t getSizeInBytes() const { return SizeInBytes; }
   int32_t getOffset() const { return Offset; }
+};
+
+class LoadSDNode : public SelectionDAGNode {
+  StackAllocSelectionDAGNode *MemLoc;
+
+ public:
+  LoadSDNode(StackAllocSelectionDAGNode *MemLoc)
+      : SelectionDAGNode(X86SelectionDAGNodeType::LOAD), MemLoc(MemLoc) {};
+};
+class StoreSDNode : public SelectionDAGNode {
+  StackAllocSelectionDAGNode *MemLoc;
+  SelectionDAGValue *ValToStore;
+
+ public:
+  StoreSDNode(StackAllocSelectionDAGNode *MemLoc, SelectionDAGValue *ValToStore)
+      : SelectionDAGNode(X86SelectionDAGNodeType::STORE),
+        MemLoc(MemLoc),
+        ValToStore(ValToStore) {}
+  void setValue(SelectionDAGValue *Val) { ValToStore = Val; }
+};
+
+// Wrapper for a node producing a runtime 'value', this could be:
+// 	- A pointer
+// 	- Result of an instruction
+class SelectionDAGValue {
+  SelectionDAGNode *Node;
+  std::string ValueName;
+
+  inline static unsigned short count = 0;
+
+  std::string constructValueName() {
+    if (Node->getType() == X86SelectionDAGNodeType::STACK_ALLOCATION) {
+      return static_cast<StackAllocSelectionDAGNode *>(Node)->getName();
+    } else {
+      std::stringstream ss;
+      ss << "%" << count++;
+      return ss.str();
+    }
+  }
+
+ public:
+  SelectionDAGValue(SelectionDAGNode *Node)
+      : Node(Node), ValueName(constructValueName()) {}
+
+  ~SelectionDAGValue() = default;
+  SelectionDAGNode *getNode() const { return Node; }
 };
 
 // Directed Acyclic Graph of Instruction Selection Nodes
@@ -116,6 +162,8 @@ class SelectionDAG {
   void SelectLoad(const llvm::LoadInst &load);
   void SelectStore(const llvm::StoreInst &store);
   void SelectBinary(const llvm::Instruction &bin);
+
+  void TryCreateConstantNode(llvm::Value *Val);
 
  public:
   SelectionDAG(const llvm::BasicBlock &MainBlock) : MainBlock(MainBlock) {
--- lib/SelectionDAG.cpp
+++ lib/SelectionDAG.cpp
@@ -2,6 +2,7 @@
 // ~~~~~~~~~~~~~~~~
 // Implmentation of the Selction DAG interface.
 #include <Edge/SelectionDAG.h>
+#include <llvm/IR/Constants.h>
 #include <llvm/IR/Instructions.h>
 #include <llvm/IR/Module.h>
 
@@ -43,12 +44,78 @@ void SelectionDAG::SelectAlloca(const llvm::AllocaInst &alloca) {
                   ->getFixedValue();
   auto N = new StackAllocSelectionDAGNode(alloca.getName(), (uint32_t)size,
                                           StackSizeInBytes);
+  StackSizeInBytes += size;
   Nodes.push_back(N);
+  ValueMap.insert(std::make_pair(&alloca, SelectionDAGValue(N)));
 }
 
-void SelectionDAG::SelectLoad(const llvm::LoadInst &load) {}
-void SelectionDAG::SelectStore(const llvm::StoreInst &store) {}
-void SelectionDAG::SelectBinary(const llvm::Instruction &I) {}
+void SelectionDAG::SelectLoad(const llvm::LoadInst &load) {
+  // Since the language is built such that all allocas are
+  // at the beginning of the 'main' basic block, we can assume
+  // that all allocas have been selected and we have a valid
+  // value to retrieve from the table.
+  SelectionDAGValue allocaVal = ValueMap.at(load.getOperand(0));
+  auto LN = new LoadSDNode(
+      static_cast<StackAllocSelectionDAGNode *>(allocaVal.getNode()));
+  auto U = new SelectionDAGUse(&allocaVal, LN);
+  ValueMap.insert(std::make_pair(&load, SelectionDAGValue(LN)));
+  LN->addOperand(U);
+  Nodes.push_back(LN);
+}
+
+void SelectionDAG::SelectStore(const llvm::StoreInst &store) {
+  // Storing doesn't return a value, so we can just create
+  // a store node here with the pointer of choice. Constant
+  // values nodes should be created here if storing an imm64.
+  auto allocaVal = ValueMap.at(store.getOperand(1));
+  auto SN = new StoreSDNode(
+      static_cast<StackAllocSelectionDAGNode *>(allocaVal.getNode()), nullptr);
+  auto ValToStore = store.getOperand(0);
+  TryCreateConstantNode(ValToStore);
+  auto SDValToStore = ValueMap.at(ValToStore);
+  SN->addOperand(new SelectionDAGUse(&allocaVal, SN));
+  SN->addOperand(new SelectionDAGUse(&SDValToStore, SN));
+  Nodes.push_back(SN);
+}
+
+void SelectionDAG::SelectBinary(const llvm::Instruction &I) {
+  using BOps = llvm::Instruction::BinaryOps;
+  auto getX86SDNodeType = [](unsigned int Op) {
+    switch (Op) {
+      case BOps::Add:
+        return X86SelectionDAGNodeType::ADD;
+      case BOps::Sub:
+        return X86SelectionDAGNodeType::SUB;
+      case BOps::Mul:
+        return X86SelectionDAGNodeType::MUL;
+      case BOps::SDiv:
+        return X86SelectionDAGNodeType::DIV;
+      default:
+        return X86SelectionDAGNodeType::UNKNOWN;
+    }
+  };
+
+  auto RHS = I.getOperand(0);
+  TryCreateConstantNode(RHS);
+  auto LHS = I.getOperand(1);
+  TryCreateConstantNode(LHS);
+
+  auto N = new SelectionDAGNode(getX86SDNodeType(I.getOpcode()));
+  Nodes.push_back(N);
+  N->addOperand(new SelectionDAGUse(&ValueMap.at(RHS), N));
+  N->addOperand(new SelectionDAGUse(&ValueMap.at(LHS), N));
+  ValueMap.insert(std::make_pair(&I, SelectionDAGValue(N)));
+}
+
+void SelectionDAG::TryCreateConstantNode(llvm::Value *Val) {
+  if ((ValueMap.find(Val) == ValueMap.end()) &&
+      llvm::isa<llvm::ConstantInt>(Val)) {
+    auto CI = static_cast<llvm::ConstantInt *>(Val);
+    auto CN = new ConstantSelectionDAGNode(CI->getValue().getLimitedValue());
+    Nodes.push_back(CN);
+    ValueMap.insert(std::make_pair(Val, SelectionDAGValue(CN)));
+  }
+}
 
 void SelectionDAG::printStackObjects() {
   llvm::outs() << "Stack Frame Objects\n-------------------\n";
